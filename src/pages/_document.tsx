45cc268d45afbb9284d80597f15150c412c990a1
Benjamin von Potobsky <bvonpotobsky@gmail.com>
2022-10-02 17:21:22 -0300

    Setup project. Exactly challenge

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import {Html, Head, Main, NextScript} from "next/document";
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;1,200;1,300;1,400;1,600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type {NextPage} from "next";
+
+const Home: NextPage = () => {
+  return (
+    <main className="w-full min-h-screen overflow-hidden border border-red-500">
+      <h1>Hello World, let&rsquo;s decentralize the time-value of money!</h1>
+    </main>
+  );
+};
+
+export default Home;
