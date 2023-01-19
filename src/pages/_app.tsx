9158e98ab9f7b403ed504714e04eaf1ef7e146c1
Benjamin von Potobsky <bvonpotobsky@gmail.com>
2023-01-20 02:04:16 +1100

    remove packs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,10 @@
 import type {AppProps} from "next/app";
 import {ThemeProvider} from "next-themes";
 
-import {WagmiConfig, createClient} from "wagmi";
-import {getDefaultProvider} from "ethers";
-
 import "@/styles/globals.css";
-
-const client = createClient({
-  autoConnect: true,
-  provider: getDefaultProvider(),
-});
 
 const MyApp = ({Component, pageProps}: AppProps) => {
   return (
-    // <WagmiConfig client={client}></WagmiConfig>
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
